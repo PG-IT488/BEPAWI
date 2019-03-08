3743fc8b7e8b48ebe90bac32e0ba29e0c5544793
eames <eames@DESKTOP-0KAFHFK>
2019-03-08 15:20:34 -0500

    Working on Inventory Management Page Design (Paul)

--- BEPAWI/src/InventoryManagementPage.java
+++ BEPAWI/src/InventoryManagementPage.java
@@ -15,11 +15,26 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JScrollPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JList;
 
 public class InventoryManagementPage extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField PcodeText;
+	private JTextField UpdatePcodeText;
+	private JTextField AddPcodeText;
+	private JTextField UpdatePnameText;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
 
 	/**
 	 * Launch the application.
@@ -45,7 +60,7 @@ public class InventoryManagementPage extends JFrame {
 		setTitle("Inventory Management Page");
 		setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 800, 662);
+		setBounds(100, 100, 1027, 662);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -59,39 +74,241 @@ public class InventoryManagementPage extends JFrame {
 		label.setBorder(new MatteBorder(0, 0, 4, 0, (Color) new Color(0, 0, 255)));
 		label.setBackground(Color.BLUE);
 		
-		PcodeText = new JTextField();
-		PcodeText.setColumns(10);
+		UpdatePcodeText = new JTextField();
+		UpdatePcodeText.setColumns(10);
 		
-		JLabel lblProductCode = new JLabel("Product Code");
-		lblProductCode.setFont(new Font("Tahoma", Font.BOLD, 24));
+		JLabel lblUpdateProductCode = new JLabel("Code:");
+		lblUpdateProductCode.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JLabel lblUpdateProduct = new JLabel("Update Product");
+		lblUpdateProduct.setFont(new Font("Tahoma", Font.BOLD, 24));
+		
+		JLabel lblUpdateAddNewItem = new JLabel("Add New Item");
+		lblUpdateAddNewItem.setFont(new Font("Tahoma", Font.BOLD, 24));
+		
+		AddPcodeText = new JTextField();
+		AddPcodeText.setColumns(10);
+		
+		JLabel lblAddProductCode = new JLabel("Code:");
+		lblAddProductCode.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JLabel lblUpdateProductName = new JLabel("Name:");
+		lblUpdateProductName.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		UpdatePnameText = new JTextField();
+		UpdatePnameText.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Name:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JButton btnNewButton = new JButton("Enter");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JButton btnNewButton_1 = new JButton("Enter");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JLabel lblProductDescription = new JLabel("Description:");
+		lblProductDescription.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Description:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JLabel lblNewLabel_2 = new JLabel("Picture:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JLabel lblNewLabel_3 = new JLabel("Picture:");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		
+		JLabel lblLocation = new JLabel("Location:");
+		lblLocation.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JList list = new JList();
+		
+		JLabel lblNewLabel_4 = new JLabel("Type:");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		
+		JLabel lblNewLabel_5 = new JLabel("Type:");
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		
+		JLabel lblNewLabel_6 = new JLabel("Pack Qnty:");
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JLabel lblNewLabel_7 = new JLabel("Pack Qnty:");
+		lblNewLabel_7.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JLabel lblNewLabel_8 = new JLabel("Price:");
+		lblNewLabel_8.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		
+		JLabel lblNewLabel_9 = new JLabel("Price:");
+		lblNewLabel_9.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		textField_9 = new JTextField();
+		textField_9.setColumns(10);
+		
+		textField_10 = new JTextField();
+		textField_10.setColumns(10);
+		
+		JButton btnNewButton_2 = new JButton("View Inventory");
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 16));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 764, Short.MAX_VALUE)
-							.addContainerGap())
 						.addComponent(label, GroupLayout.PREFERRED_SIZE, 774, GroupLayout.PREFERRED_SIZE)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblProductCode)
-							.addContainerGap(699, Short.MAX_VALUE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(PcodeText, GroupLayout.PREFERRED_SIZE, 246, GroupLayout.PREFERRED_SIZE)
-							.addContainerGap(528, Short.MAX_VALUE))))
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(AddPcodeText, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblAddProductCode, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE))
+									.addGap(21)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblNewLabel)
+										.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE))
+									.addGap(18)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+										.addComponent(textField_2)
+										.addComponent(lblNewLabel_1)))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+										.addComponent(lblUpdateAddNewItem, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 201, GroupLayout.PREFERRED_SIZE)
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addComponent(UpdatePcodeText, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+											.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+											.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+												.addComponent(lblUpdateProductName, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+												.addComponent(UpdatePnameText, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE))))
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblProductDescription)
+										.addComponent(textField, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE)))
+								.addComponent(lblUpdateProductCode, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE))
+							.addGap(18)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNewLabel_3)
+								.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNewLabel_2))
+							.addGap(18)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+									.addComponent(textField_5)
+									.addComponent(lblNewLabel_5)
+									.addComponent(textField_6, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addComponent(lblNewLabel_4))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+									.addComponent(textField_8, 132, 132, 132)
+									.addComponent(textField_7, Alignment.TRAILING, 132, 132, 132)
+									.addComponent(lblNewLabel_7))
+								.addComponent(lblNewLabel_6))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(lblNewLabel_9)
+								.addComponent(btnNewButton, 0, 0, Short.MAX_VALUE)
+								.addComponent(textField_9, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNewLabel_8)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(1)
+									.addComponent(textField_10, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+						.addComponent(lblUpdateProduct, GroupLayout.PREFERRED_SIZE, 201, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnNewButton_2)
+						.addComponent(list, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblLocation)
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 980, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(11, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addComponent(label, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 147, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblLocation)
+					.addPreferredGap(ComponentPlacement.RELATED, 13, Short.MAX_VALUE)
+					.addComponent(list, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnNewButton_2, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
 					.addGap(18)
-					.addComponent(lblProductCode)
+					.addComponent(lblUpdateProduct, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(PcodeText, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(323, Short.MAX_VALUE))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblUpdateProductCode)
+						.addComponent(lblUpdateProductName, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblProductDescription, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_2)
+						.addComponent(lblNewLabel_4)
+						.addComponent(lblNewLabel_6)
+						.addComponent(lblNewLabel_8))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(UpdatePcodeText, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(UpdatePnameText, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_7, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_9, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(38)
+							.addComponent(lblUpdateAddNewItem, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblAddProductCode, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_1)
+						.addComponent(lblNewLabel)
+						.addComponent(lblNewLabel_3)
+						.addComponent(lblNewLabel_5)
+						.addComponent(lblNewLabel_9)
+						.addComponent(lblNewLabel_7))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(AddPcodeText, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_6, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_10, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_8, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
+					.addGap(6))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
